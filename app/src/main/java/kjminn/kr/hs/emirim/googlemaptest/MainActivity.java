8f7ebeaf2bb8d0fe5cf9e60a5add842da5f01d08
justhis01010101 <offentsive@naver.com>
2017-06-27 20:04:31 +0900

    #5

--- app/src/main/java/kjminn/kr/hs/emirim/googlemaptest/MainActivity.java
+++ app/src/main/java/kjminn/kr/hs/emirim/googlemaptest/MainActivity.java
@@ -2,6 +2,9 @@ package kjminn.kr.hs.emirim.googlemaptest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.SubMenu;
 
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -20,7 +23,6 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         setContentView(R.layout.activity_main);
         mapFragment=(SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-
     }
 
     @Override
@@ -30,5 +32,54 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         //다른지역으로 바꾸어주기
         googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(37.512149,126.833120), 17)); //17==확대 축소 레밸
         googleMap.getUiSettings().setZoomControlsEnabled(true); //확대축소 버튼
+    }
+    public static final  int ITEM_SATELLITE=1;
+    public static final  int ITEM_NOMAL=2;
+    public static final  int ITEM_PODO=3;
+    public static final  int ITEM_LOTTE=4;
+
+
+    @Override
+    public int hashCode() {
+        int result = googleMap != null ? googleMap.hashCode() : 0;
+        result = 31 * result + (mapFragment != null ? mapFragment.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+      super.onCreateOptionsMenu(menu);
+      menu.add(0,ITEM_SATELLITE,0,"위성지도");
+        menu.add(0,ITEM_NOMAL,0,"일반지도");
+        SubMenu hotMenu= menu.addSubMenu("핫 플레이스");
+        hotMenu.add(0,ITEM_PODO,0,"포도몰");
+        hotMenu.add(0,ITEM_LOTTE,0,"롯데월드");
+
+
+//        menu.add(0,ITEM_PODO,0,"포도몰");
+      return true;
+
+  }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+      super.onOptionsItemSelected(item);
+        switch (item.getItemId())
+        {
+            case ITEM_SATELLITE:
+                googleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+                return true;
+            case ITEM_NOMAL:
+                googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                return true;
+            case ITEM_PODO:
+                googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(37.483934, 126.930064),17));
+                return true;
+            case ITEM_LOTTE:
+                googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(37.511303, 127.098199),17));
+                return true;
+        }
+        return false; //항목이 선택되었을 때만 id 별로
+
     }
 }
